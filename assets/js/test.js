6da5a3329a44b76c578bf85664c0a4952f34efb4
michaelcounseller <michaelcounseller@gmail.com>
2022-09-12 09:18:47 -0500

    correctly added a enemyhealth value reset when changing enemies by skipping or defeating them, added a shop functionality allowing the user to heal or upgrade their attack power

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,32 +1,25 @@
 const playerName = prompt("What is your name?");
-
 console.log(`Fighters name is ${playerName}`);
 
 let playerHealth = 100; // Not redeclaring in the parameter so the value keeps changing
-const playerAttack = 10;
-let playerMoney = 10
+let playerAttack = 10;
+let playerMoney = 10;
 
 let enemyNames = ["Salem", "Kitty", "Pumpkin"];
 
 const enemyAttack = 12;
 let enemyHealth = 50;
 
-
-function battle(
-  playerAttack,
-  playerName,
-  enemyAttack,
-  enemyHealth,
-  enemyNames
-) {
+let battle = function (playerName, enemyNames) {
   while (playerHealth > 0 && enemyHealth > 0) {
     const promptFight = prompt("Do you want to FIGHT or SKIP");
     if (promptFight === "skip" || promptFight === "SKIP") {
       let confirmSkip = confirm("Do you really wish to skip?");
       if (confirmSkip) {
-        playerMoney--
-        console.log(playerMoney)
+        playerMoney--;
+        console.log(playerMoney);
         console.log("The player will skip");
+        shop();
         break;
       }
     }
@@ -40,6 +33,7 @@ function battle(
 
     if (enemyHealth <= 0) {
       console.log(`${enemyNames} has been defeated`);
+      shop();
       break;
     }
 
@@ -57,16 +51,87 @@ function battle(
 
     console.log(playerHealth, enemyHealth);
   }
-}
+};
 
-for (i = 0; i < enemyNames.length; i++) {
-  debugger
-  
-  console.log(enemyNames[i])
-  console.log(i)
-  let currentEnemy = enemyNames[i]
-  console.log(currentEnemy)
-  console.log(enemyNames[i])
-  console.log(i)
-  battle(playerAttack, playerName, enemyAttack, enemyHealth, currentEnemy);
-}
+let startGame = function (playerName, enemyNames) {
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      alert(`Let the battle begin! Round ${i + 1}`);
+      let currentEnemy = enemyNames[i];
+
+      enemyHealth = 50;
+
+      battle(playerName, currentEnemy);
+    } else {
+      alert("You have died");
+      endGame();
+      break;
+    }
+  }
+
+  endGame();
+};
+
+let endGame = function () {
+  if (playerHealth > 0) {
+    alert(
+      `You survived the game! Your score is how much money you earned. And you have earned...${playerMoney}!!`
+    );
+    let restartPrompt = confirm("Would you like to play again?");
+    if (restartPrompt) {
+      startGame();
+    } else {
+      alert("Thanks for playing!");
+    }
+  } else {
+    alert("You have died! Game over.");
+    let restartPrompt = confirm("Would you like to play again?");
+
+    if (restartPrompt) {
+      startGame();
+    } else {
+      alert("Thanks for playing!");
+    }
+  }
+};
+
+let shop = function () {
+  let shopOptions = prompt(
+    "Would you like to heal with a POTION or UPGRADE your magical weapoon? Please enter one of the choices: POTION, UPGRADE or LEAVE"
+  );
+
+  switch (shopOptions) {
+    case "attack":
+      if (playerMoney >= 2) {
+        playerMoney -= 2;
+        playerAttack += 2;
+      } else {
+        alert("'You can't afford that!")
+        shop()
+      }
+
+      console.log(`money ${playerMoney}, ${playerAttack}`);
+      break;
+    case "potion":
+      if (playerMoney >= 2) {
+        playerMoney -= 2;
+        playerHealth += 2;
+      } else {
+        alert("You can't afford that!")
+        shop();
+      }
+
+      console.log(`money ${playerMoney}, ${playerHealth}`);
+      break;
+    case "leave":
+      break;
+    default:
+      shop();
+  }
+};
+
+startGame(playerName, enemyNames);
