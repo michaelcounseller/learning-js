c0c31e9da719d4ab92582d344011288746ce8554
michaelcounseller <michaelcounseller@gmail.com>
2022-09-12 00:16:12 -0500

    added a check to see if the enemy is alive or not so the enemy dead still doesnt get a hit in and a prompt asking the challenger if they wish to fight or skip

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,7 +2,7 @@ const playerName = prompt("What is your name?");
 
 console.log(`Fighters name is ${playerName}`);
 
-let playerHealth = 100;
+let playerHealth = 100; // Not redeclaring in the parameter so the value keeps changing
 let playerAttack = 10;
 
 let enemyNames = ["Salem", "Kitty", "Pumpkin"];
@@ -11,40 +11,51 @@ let enemyAttack = 12;
 let enemyHealth = 50;
 
 function battle(
-  playerName,
   playerAttack,
-  enemyNames,
+  playerName,
   enemyAttack,
-  enemyHealth
+  enemyHealth,
+  enemyNames
 ) {
-  while (enemyHealth > 0) {
+  while (playerHealth > 0 && enemyHealth > 0) {
+    enemyHealth -= playerAttack;
+    console.log(
+      `${playerName} hit ${enemyNames} for ${playerAttack} leaving ${enemyNames} with ${enemyHealth}`
+    );
+
+    console.log(enemyHealth);
+
+    if (enemyHealth <= 0) {
+      console.log(`${enemyNames} has been defeated`);
+      break;
+    }
+
     playerHealth -= enemyAttack;
     console.log(
       `${enemyNames} hit ${playerName} for ${enemyAttack} leaving ${playerName} with ${playerHealth}`
     );
+
+    console.log(playerHealth);
 
     if (playerHealth <= 0) {
       console.log("You have been defeated");
       break;
     }
 
-    console.log(playerHealth);
-
-    enemyHealth -= playerAttack;
-    console.log(
-      `${playerName} hit ${enemyNames} for ${playerAttack} leaving ${enemyNames} with ${enemyHealth}`
-    );
+    console.log(playerHealth, enemyHealth);
   }
 }
 
 for (i = 0; i < enemyNames.length; i++) {
+  battle(playerAttack, playerName, enemyAttack, enemyHealth, enemyNames[i]);
 
-
-  battle(
-    playerName,
-    playerAttack,
-    enemyNames[i],
-    enemyAttack,
-    enemyHealth
-  );
+  if (playerHealth > 0) {
+    const askPrompt = prompt("Do you want to FIGHT or SKIP");
+    switch (askPrompt) {
+      case "SKIP":
+      case "skip":
+        console.log("Player loses money");
+        break;
+    }
+  }
 }
