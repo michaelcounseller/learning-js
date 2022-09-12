fcf55d7271a322866be2d103538b4cfeaef738c1
michaelcounseller <michaelcounseller@gmail.com>
2022-09-12 02:53:57 -0500

    added a prompt to be ask if the player wishes to continue fighting the current enemy or skip to the next enemy but lose gold.  currently figuring out why declaring a new variable and having its value of enemysName[i] works the way i want it to, otherwise it would just skip the attack sequence rather than the enemy

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,49 +2,71 @@ const playerName = prompt("What is your name?");
 
 console.log(`Fighters name is ${playerName}`);
 
-let playerHealth = 100;
-let playerAttack = 10;
+let playerHealth = 100; // Not redeclaring in the parameter so the value keeps changing
+const playerAttack = 10;
+let playerMoney = 10
 
 let enemyNames = ["Salem", "Kitty", "Pumpkin"];
 
-let enemyAttack = 12;
+const enemyAttack = 12;
 let enemyHealth = 50;
 
+
 function battle(
-  playerName,
   playerAttack,
-  enemyNames,
+  playerName,
   enemyAttack,
-  enemyHealth
+  enemyHealth,
+  enemyNames
 ) {
-  while (enemyHealth > 0) {
+  while (playerHealth > 0 && enemyHealth > 0) {
+    const promptFight = prompt("Do you want to FIGHT or SKIP");
+    if (promptFight === "skip" || promptFight === "SKIP") {
+      let confirmSkip = confirm("Do you really wish to skip?");
+      if (confirmSkip) {
+        playerMoney--
+        console.log(playerMoney)
+        console.log("The player will skip");
+        break;
+      }
+    }
+
+    enemyHealth -= playerAttack;
+    console.log(
+      `${playerName} hit ${enemyNames} for ${playerAttack} leaving ${enemyNames} with ${enemyHealth}`
+    );
+
+    console.log(enemyHealth);
+
+    if (enemyHealth <= 0) {
+      console.log(`${enemyNames} has been defeated`);
+      break;
+    }
+
     playerHealth -= enemyAttack;
     console.log(
       `${enemyNames} hit ${playerName} for ${enemyAttack} leaving ${playerName} with ${playerHealth}`
     );
+
+    console.log(playerHealth);
 
     if (playerHealth <= 0) {
       console.log("You have been defeated");
       break;
     }
 
-    console.log(playerHealth);
-
-    enemyHealth -= playerAttack;
-    console.log(
-      `${playerName} hit ${enemyNames} for ${playerAttack} leaving ${enemyNames} with ${enemyHealth}`
-    );
+    console.log(playerHealth, enemyHealth);
   }
 }
 
 for (i = 0; i < enemyNames.length; i++) {
-
-
-  battle(
-    playerName,
-    playerAttack,
-    enemyNames[i],
-    enemyAttack,
-    enemyHealth
-  );
+  debugger
+  
+  console.log(enemyNames[i])
+  console.log(i)
+  let currentEnemy = enemyNames[i]
+  console.log(currentEnemy)
+  console.log(enemyNames[i])
+  console.log(i)
+  battle(playerAttack, playerName, enemyAttack, enemyHealth, currentEnemy);
 }
